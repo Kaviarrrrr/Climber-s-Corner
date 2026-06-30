b0c17a5f2af447c98ede28e6acace5db1a869125
Kaviarrrrr <2gdpr7rt96@privaterelay.appleid.com>
2026-06-30 16:42:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ if (toggle && nav) {
     nav.classList.toggle("open");
   });
 }
+
+const gymSearch = document.querySelector("#gymSearch");
+const gymCards = document.querySelectorAll(".review-card");
+
+if (gymSearch && gymCards.length > 0) {
+  gymSearch.addEventListener("input", () => {
+    const searchText = gymSearch.value.toLowerCase().trim();
+
+    gymCards.forEach((card) => {
+      const cardText = card.textContent.toLowerCase();
+      const isMatch = cardText.includes(searchText);
+
+      card.style.display = isMatch ? "" : "none";
+    });
+  });
+}
